62b49fbc423510d831b204e4a2294373d6a81359
Joshua Snider <jsnider1938@gmail.com>
2014-08-17 15:32:00 -0700

    methods.rb

--- chapter_methods/methods.rb
+++ chapter_methods/methods.rb
@@ -1,15 +1,6 @@
-# return.rb
-
-def add_three(number)
-	return number += 3
-	number += 4
+def scream(words)
+	words = words + "!!!!"
+	puts words
 end
 
-returned_value = add_three(4)
-puts returned_value
-
-def just_assignment(number)
-	foo = number + 3
-end
-
-puts just_assignment(2)
+scream("Yippeeee")
