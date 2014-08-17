0b5b98d9c18db84163c9d420c2ef0a7d84af960f
Joshua Snider <jsnider1938@gmail.com>
2014-08-16 20:59:20 -0700

    same as before but in an array

--- basics.rb
+++ basics.rb
@@ -1,8 +1,13 @@
-movies = {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}
-puts movies[:barry_lyndon]
-puts movies[:closer]
-puts movies[:all_is_lost]
-puts movies[:amelie]
-puts movies[:atlantic_city]
+# movies = {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}
+# puts movies[:barry_lyndon]
+# puts movies[:closer]
+# puts movies[:all_is_lost]
+# puts movies[:amelie]
+# puts movies[:atlantic_city]
 
-#test
+movies = [1975, 2004, 2013 2001, 1981]
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
