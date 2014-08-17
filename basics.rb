c902e47c047159f09790c691253880298d6ab159
Joshua Snider <jsnider1938@gmail.com>
2014-08-16 20:43:35 -0700

    the question is...

--- basics.rb
+++ basics.rb
@@ -1,5 +1,8 @@
-puts {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}[:barry_lyndon]
-puts {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}[:closer]
-puts {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}[:all_is_lost]
-puts {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}[:amelie]
-puts {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}[:atlantic_city]
+movies = {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}
+puts movies[:barry_lyndon]
+puts movies[:closer]
+puts movies[:all_is_lost]
+puts movies[:amelie]
+puts movies[:atlantic_city]
+
+#test
