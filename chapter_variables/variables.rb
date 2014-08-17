24ff9f068952416e3b0a8cd8b6acc19d9d1b7b0c
Joshua Snider <jsnider1938@gmail.com>
2014-08-17 14:52:05 -0700

    changed variables.rb

--- chapter_variables/variables.rb
+++ chapter_variables/variables.rb
@@ -1,23 +1,8 @@
-#name.rb
-
-# p "How old are you?"
-
-# age = gets.chomp.to_i
-
-# p "In 10 years you will be"
-# p age + 10
-# p "In 20 years you will be"
-# p age + 20
-# p "In 30 years you will be"
-# p age + 30
-# p "In 40 years you will be"
-# p age + 40
-# p "In 50 years you will be"
-# p age + 50
-
-p "What's your name again?"
-name = gets.chomp
-puts "This is your name ten times. Why? idk...because lolz"
-10.times do |n|
-	p name
+y = 0
+3.times do |x|
+	y += 1
+	x = y
 end
+
+p y
+p x
