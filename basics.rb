763deb6ad1ac26a8717cd98431a93bafe552e0a8
Joshua Snider <jsnider1938@gmail.com>
2014-08-16 21:02:19 -0700

    outputs factorial

--- basics.rb
+++ basics.rb
@@ -1,13 +1,4 @@
-# movies = {:barry_lyndon => 1975, :closer => 2004, :all_is_lost => 2013, :amelie => 2001, :atlantic_city => 1981}
-# puts movies[:barry_lyndon]
-# puts movies[:closer]
-# puts movies[:all_is_lost]
-# puts movies[:amelie]
-# puts movies[:atlantic_city]
-
-movies = [1975, 2004, 2013 2001, 1981]
-puts movies[0]
-puts movies[1]
-puts movies[2]
-puts movies[3]
-puts movies[4]
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
