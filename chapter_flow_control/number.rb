754254a141ae3e42b6478cd99a25deeb9f0673e7
Joshua Snider <jsnider1938@gmail.com>
2014-08-20 17:13:54 -0700

    number.rb

--- chapter_flow_control/number.rb
+++ chapter_flow_control/number.rb
@@ -1,4 +1,4 @@
-# number.rb
+# number.rb -tweaked to be more succint
 
 # will take a number from the user between 0 and 100 and reports back 
 # whether the number is between 0 and 50, 50 and 100, or above 100
@@ -6,14 +6,14 @@
 def get_num()
 	p "Give us a number, precious.  But make it between 0 and 100"
 	num = gets.chomp.to_i
-	case
-	when num < 0 || num > 100
-		puts "filthy hobbitses!! only between 0 and 100 precious, that's what we said."
-		get_num()
-	when num >= 0 && num <= 50
+	case num
+	when 0..50
 		puts "the number is between 0 and 50!!"
-	when num > 50 && num <= 100
+	when 51..100
 		puts "the number is between 51 and 100!!"
+	else
+		puts "filthy hobbitses!! a number between 0 and 100, precious, that's what we said."
+		get_num()
 	end
 end
 
