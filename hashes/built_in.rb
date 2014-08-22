fc96466b2d76a71e4c8c956dca060ddd13effa94
Joshua Snider <jsnider1938@gmail.com>
2014-08-22 13:30:03 -0700

    built_in.rb

--- hashes/built_in.rb
+++ hashes/built_in.rb
@@ -3,15 +3,9 @@
 # this program will loop through a hash and print all of the keys. Then it will do the same thing
 # but print the values.  Then it will do both.
 
-hash = {animal: 'cat', weight: '5 lbs', color: 'black'}
-puts "This is the hash: #{hash}.  I will now print all of the keys"
-keys = hash.keys
-puts keys
-puts "-------------------------------"
-puts "Now I will print all of the values."
-values = hash.values
-puts values
-puts "-------------------------------"
-puts "Now I will print both the keys and values"
-keys_and_values = keys.product(values).flatten.uniq
-puts keys_and_values
+hash = {name: 'cupcake', weight: '5 lbs', color: 'black'}
+
+p hash.keys
+p hash.values
+
+hash.select { |k, v| puts "Her #{k} is #{v}"}
