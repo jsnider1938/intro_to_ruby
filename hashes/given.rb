24a3a64d2bcf37844accf51c3020f513fde9e3b7
Joshua Snider <jsnider1938@gmail.com>
2014-08-22 13:06:58 -0700

    given.rb

---
+++ hashes/given.rb
@@ -0,0 +1,11 @@
+# given.rb
+
+family = { uncles: ['bob', 'joe', 'steve'],
+           sisters: ['jane', 'jill', 'beth'],
+           brothers: ['frank', 'rob', 'david'],
+           aunts: ['mary', 'sally', 'susan']
+          }
+
+immediate_family = family.select { |k, v| k == :sisters || k == :brothers}
+immediate_family = immediate_family.values.flatten
+puts immediate_family
