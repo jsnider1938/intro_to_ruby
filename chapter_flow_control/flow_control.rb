86b8c72046246414a9d6fc2ea2656fa4e6295925
Joshua Snider <jsnider1938@gmail.com>
2014-08-22 12:53:47 -0700

    flow_control.rb

--- chapter_flow_control/flow_control.rb
+++ chapter_flow_control/flow_control.rb
@@ -1,13 +1,7 @@
-# case_tatement.rb
+a = false
 
-a = 5
-
-case a
-	when 5
-		puts "a is 5"
-	when 6
-		puts "a is 6"
-	else
-		puts "a is neither 5 nor 6"
-	end
-
+if a
+	puts "how can this be true"
+else
+	puts "this is not true"
+end
--- chapter_flow_control/number.rb
+++ chapter_flow_control/number.rb
@@ -1,4 +1,4 @@
-# number.rb -tweaked to be more succint
+# number.rb -tweaked to be more succintc
 
 # will take a number from the user between 0 and 100 and reports back 
 # whether the number is between 0 and 50, 50 and 100, or above 100
